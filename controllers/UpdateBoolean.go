745c8f997441bbb346a8faafd36e8ea925b8ba85
Raghav Tayal <raghavtayal2@gmail.com>
2020-09-24 17:33:20 +0530

    Update UpdateBoolean.go

--- controllers/UpdateBoolean.go
+++ controllers/UpdateBoolean.go
@@ -2,7 +2,7 @@ package controllers
 
 import (
 	"Go_boolean_service/db"
-	"strconv"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -10,13 +10,18 @@ import (
 
 //UpdateBoolean updates the boolean value after Authorisation with token and given valid ID
 func UpdateBoolean(database *gorm.DB) func(*gin.Context) {
+	var boolObj db.BooleanTemp
 	return func(c *gin.Context) {
 		if AuthCheck(c) {
 			return
 		}
 		ID := c.Param("id")
-		Value, _ := strconv.ParseBool(c.PostForm("value"))
-		db.UpdateBoolean(database, ID, c.PostForm("key"), Value)
-
+		if err := c.ShouldBindJSON(&boolObj); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		db.UpdateBoolean(database, ID, boolObj)
+		boolObj := db.ReadBoolean(database, ID)
+		c.JSON(http.StatusOK, gin.H{"id": boolObj.Id, "key": boolObj.Key, "value": boolObj.Value})
 	}
 }
