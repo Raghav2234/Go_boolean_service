54da49fdf3163f3da2ce8dfca717b301f787d76f
Raghav Tayal <raghavtayal2@gmail.com>
2020-09-24 17:31:44 +0530

    Update CreateBoolean.go

--- controllers/CreateBoolean.go
+++ controllers/CreateBoolean.go
@@ -3,9 +3,8 @@ package controllers
 import (
 	"Go_boolean_service/auth"
 	"Go_boolean_service/db"
+	"fmt"
 	"net/http"
-	"strconv"
-	"strings"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -14,13 +13,16 @@ import (
 //CreateBoolean creates the boolean provided key-value pair and returns the identifier(ID)
 func CreateBoolean(database *gorm.DB) func(*gin.Context) {
 	return func(c *gin.Context) {
-		if (strings.ToLower(c.PostForm("value")) != "true") && (strings.ToLower(c.PostForm("value")) != "false") {
-			c.JSON(http.StatusNotAcceptable, gin.H{"error": "bool value required"})
+		var boolObj db.BooleanTemp
+		if err := c.ShouldBindJSON(&boolObj); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
-		value, _ := strconv.ParseBool(strings.ToLower(c.PostForm("value")))
-		boolObj := db.CreateBoolean(database, c.PostForm("key"), value)
-		token, _ := auth.GenerateAccessToken(boolObj.Id)
-		c.JSON(http.StatusOK, gin.H{"token": token, "id": boolObj.Id, "key": boolObj.Key, "value": boolObj.Value})
+
+		resp := db.CreateBoolean(database, boolObj)
+		token, _ := auth.GenerateAccessToken(resp.Id)
+		obj := db.ReadBoolean(database, resp.Id)
+		fmt.Println(obj.Id, obj.Key, obj.Value)
+		c.JSON(http.StatusOK, gin.H{"token": token, "id": resp.Id, "key": resp.Key, "value": resp.Value})
 	}
 }
