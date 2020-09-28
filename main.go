951d04b25f27bc2715efaa7b81deae6dac2f39f2
Raghav Tayal <raghavtayal2@gmail.com>
2020-09-28 06:51:21 +0530

    Update main.go

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"Go_boolean_service/controllers"
 	"Go_boolean_service/db"
+	"Go_boolean_service/queue"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,12 +13,15 @@ func main() {
 }
 func serverSetup() *gin.Engine {
 	database, err := db.CreateConnection()
+
+	jobChan := queue.MessageQueue(database)
+
 	if err != nil {
 		panic("Database not connected")
 	}
 	r := gin.Default()
 	r.GET("/:id", controllers.GetBoolean(database))
-	r.POST("/", controllers.CreateBoolean(database))
+	r.POST("/", controllers.CreateBoolean(database, jobChan))
 	r.PATCH("/:id", controllers.UpdateBoolean(database))
 	r.DELETE("/:id", controllers.DeleteBoolean(database))
 	return r // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
