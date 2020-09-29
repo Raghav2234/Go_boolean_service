dd703c98212072a48f2fe0e76a18cdd0da19fc35
Raghav Tayal <raghavtayal2@gmail.com>
2020-09-29 10:40:44 +0530

    Update CreateBoolean.go

--- controllers/CreateBoolean.go
+++ controllers/CreateBoolean.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"Go_boolean_service/auth"
 	"Go_boolean_service/db"
+	"Go_boolean_service/models"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -10,20 +11,18 @@ import (
 )
 
 //CreateBoolean creates the boolean provided key-value pair and returns the identifier(ID)
-func CreateBoolean(database *gorm.DB, jobChan chan db.Boolean) func(*gin.Context) {
+func CreateBoolean(database *gorm.DB, jobChan chan models.Boolean) func(*gin.Context) {
 	return func(c *gin.Context) {
-		var obj db.BooleanTemp
+		var obj models.BooleanTemp
 		if err := c.ShouldBindJSON(&obj); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
 		ID := db.CreateUUID()
-		boolObj := db.Boolean{Id: ID, Key: obj.Key, Value: obj.Value}
+		boolObj := models.Boolean{Id: ID, Key: obj.Key, Value: obj.Value}
 		jobChan <- boolObj
-		// resp := db.CreateBoolean(database, boolObj)
 		token, _ := auth.GenerateAccessToken(ID)
-		// obj := db.ReadBoolean(database, resp.Id)
-		// fmt.Println(obj.Id, obj.Key, obj.Value)
+
 		c.JSON(http.StatusOK, gin.H{"token": token, "id": ID, "key": boolObj.Key, "value": boolObj.Value})
 	}
 }
